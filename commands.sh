acd99b7d4b170b8a508150d8c3ac50761fd62435
Antoine Couprie <acouprie@hotmail.fr>
2021-09-30 18:37:16 +0200

    add commands.sh

---
+++ commands.sh
@@ -0,0 +1,5 @@
+git clone git@github.com:acouprie/udacity-azure-project2.git
+az webapp up -n udacity-azure-project2 -l eastus --sku B1
+chmod +x make_predict_azure_app.sh && ./make_predict_azure_app.sh
+az webapp log tail -g acouprie_rg_Linux_eastus -n udacity-azure-project2
+az group delete -n acouprie_rg_Linux_eastus
