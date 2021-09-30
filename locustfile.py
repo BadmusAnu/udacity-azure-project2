16e83ef343edc260524c74783e2502d8c71ab2b0
Antoine Couprie <acouprie@hotmail.fr>
2021-09-30 18:56:34 +0200

    update locust

--- locustfile.py
+++ locustfile.py
@@ -4,9 +4,9 @@ class WebsiteTestUser(HttpUser):
     wait_time = between(0.5, 3.0)
 
     @task(1)
-    def test1(self):
-        self.client.get("http://localhost:5000")
+    def test_root(self):
+        self.client.get("")
 
     @task(2)
-    def test2(self):
-        self.client.post("http://localhost:5000/predict")
+    def test_predict(self):
+        self.client.post("predict")
